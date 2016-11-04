aa24e3323b4fe49adaae259f962102d13b9013bb
Lars Kajes <lars@kajes.se>
2016-11-04 16:42:13 +0100

    Implemented empty check and sanitized input

--- functions.php
+++ functions.php
@@ -1,8 +1,20 @@
 <?php
 
+// Global requires
 require_once './lib/db.php';
 
 // Login form validation
 function loginValidate($username, $password) {
+
+    // Check if both fields are filled. Otherwise return error message
+    if ($username === '' || $password === '') {
+        return '<p>Var vänlig fyll i båda fälten.</p>';
+        die();
+    };
+
+    // Filter input from bad hacking attempts
+    $usernameSanitized = filter_var($username, FILTER_SANITIZE_STRING);
+    $passwordSanitized = filter_var($password, FILTER_SANITIZE_STRING);
+
     
 }
