1aad57fddd999d1defd9ded9abad07954794dff8
Lars Kajes <lars@kajes.se>
2016-11-07 14:00:55 +0100

    Added sanitize and some structure

--- index.php
+++ index.php
@@ -13,14 +13,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sanitize.css/2.0.0/sanitize.min.css">
+    <link rel="stylesheet" href="./css/style.css">
 	<title>Milega yManager</title>
 </head>
 <body>
-    <form action="index.php" name="loginForm" method="POST">
-        <input type="text" name="username">
-        <input type="password" name="password">
-        <input type="submit" value="Logga in">
-    </form>
+    <main class="mainWrapper">
+        <header class="mainHeader">
+            <a href="./"><h1 class="logo">Milega</h1></a>
+        </header>
+        <section class="mainContent">
+            <form action="index.php" name="loginForm" method="POST">
+                <input type="text" name="username">
+                <input type="password" name="password">
+                <input type="submit" value="Logga in">
+            </form>
+        </section>
+    </main>
     <?php
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if ($validation !== TRUE) {
