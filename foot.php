75e850e13b05f94777bc6dea510fcc088a777196
Lars Karlsson <lars@kajes.se>
2016-11-08 21:34:56 +0100

    Reworked templating for simplification

--- foot.php
+++
@@ -1,3 +0,0 @@
-<?php require_once './template/footer.php'; ?>
-</body>
-</html>
--- index.php
+++ index.php
@@ -1,6 +1,30 @@
-<?php
+<?php require_once './functions.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sanitize.css/2.0.0/sanitize.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+	<title>Milega yManager</title>
+</head>
+<body>
+    <main class="mainWrapper">
+    <?php
+        require_once './template/header.php'; // Get the header
 
-require_once './functions.php';
-require_once './head.php';
-require_once './main.php';
-require_once './template/footer.php';
+        if (!isset($_SESSION['isLoggedIn'])) {
+            include './template/loginform.php'; // Send to login page if user is not logged in
+
+        } elseif ($_SESSION['userRole'] === 1) {
+            include './template/adminpanel.php'; // If user has admin role, send to adminpanel
+
+        } elseif ($_SESSION['userRole'] === 2) {
+            include './template/form.php'; } // If user has user role, send to form
+
+        require_once './template/footer.php' // Get the footer
+    ?>
+    </main>
+</body>
+</html>
